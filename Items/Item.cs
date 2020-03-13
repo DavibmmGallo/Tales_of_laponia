56b3671148362e1a7f597ee7919f41e62cd952ad
Lhimbo <32436536+Lhimbo@users.noreply.github.com>
2020-03-13 11:37:48 -0300

    Add files via upload

---
+++ Items/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _Enter_name__backend.Items
+{
+    public class Item
+    {
+        public int ID { get; protected set; }
+        public double dmg { get; set; } // weapons and stuff
+        public string rarity { get; set; } // just for front
+        public double Mana_increase { get; set; } // magical stuff
+        public bool can_break_obj { get; set; }// mine aaaaaaaaaa [tools only]
+    }
+}
